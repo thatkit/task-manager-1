1cbdace4ade6a28648a443ce17d1c26e89160930
Nikita <emper137137@gmail.com>
2021-09-16 08:44:29 +1000

    Add class component onSubmit, etc. modifications

--- src/components/Add/Add.js
+++ src/components/Add/Add.js
@@ -1,10 +1,36 @@
 import React from 'react';
 
-export const Add = (props) => {
-    return (
-        <form className="add-task row justify-content-between">
-            <input onChange={props.handleChange} className="add-task__task-text form-control col-12" type="text" name="" id="" value=" " placeholder="New task" />
-            <input onClick={props.handleAdd} className="add-task__add-btn btn btn-success col-2" type="button" value="Add" />
-        </form>
-    );
+export class Add extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({value: e.target.value})
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(`The event target: ${e.target.value}`);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} className="add-task row justify-content-between">
+                <input
+                    onChange={this.handleChange} 
+                    className="add-task__task-text form-control col-12" 
+                    type="text" 
+                    name="" 
+                    id="" 
+                    value={this.state.value} 
+                    placeholder="New task"    
+                />
+                <input className="add-task__add-btn btn btn-success col-2" type="submit" value="Add" />
+            </form>
+        );
+    }
 }
