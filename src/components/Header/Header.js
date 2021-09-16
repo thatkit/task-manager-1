c253c0664d522904608a6ad9c2cc6b50168b3d68
Nikita <emper137137@gmail.com>
2021-09-16 19:59:14 +1000

    h1 heading centering

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Header = () => {
     return (
         <div className="row">
-            <h1 className="col-12 h1">
+            <h1 className="col-12 h1 d-flex justify-content-center">
                 ToDo's
             </h1>
         </div>
